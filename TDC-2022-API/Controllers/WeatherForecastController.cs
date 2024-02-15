0ff32f989c81e0a355ab23822d3a400a8d377ed7
matheuslorram <matheus.dumont23@hotmail.com>
2024-02-15 11:59:02 -0300

    Atualização

---
+++ TDC-2022-API/Controllers/WeatherForecastController.cs
@@ -0,0 +1,42 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace TDC_2022_API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("~/GetWeatherForecast")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        [HttpGet("~/GetIp")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(string))]
+        public IActionResult GetIp()
+        {
+            string host = System.Net.Dns.GetHostName();
+            string ip = System.Net.Dns.GetHostEntry(host).AddressList[0].ToString();
+            return Ok(ip);
+        }
+    }
+}
